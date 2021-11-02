30a14d20cd38a61b3bfedb83cd93fd56e552b903
Krysia Masińska <krysiamasinska@gmail.com>
2021-11-02 21:49:44 +0100

    Created section two with mail list

--- mian.js
+++ mian.js
@@ -1,8 +1,10 @@
+const nav = document.querySelector('.nav');
+const allNavItems = document.querySelectorAll('.nav__item');
 const search = document.querySelector('.fa-search');
 const headerInput = document.querySelector('.header__input');
 const headerImg = document.querySelector('.header__img');
 const mediaQuery = window.matchMedia('max-widht: 576px');
-
+const headerBtn = document.querySelector('.header__btn');
 
 search.addEventListener('click', () =>{
     headerInput.classList.toggle('header__addSearch');
@@ -12,4 +14,14 @@ search.addEventListener('click', () =>{
     else{
         headerImg.classList.remove('header__imgHidden')
     }
+})
+
+
+headerBtn.addEventListener('click', () =>{
+    nav.classList.toggle('showNav')
+    allNavItems.forEach(item =>{
+        item.addEventListener('click', () =>{
+            nav.classList.remove('showNav');
+        })
+    })
 })
